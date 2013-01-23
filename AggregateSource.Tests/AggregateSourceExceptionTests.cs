03a3d0fc8eb6586e0698bf83c39657f0f02931cb
Yves Reynhout <yves.reynhout@gmail.com>
2013-01-23 12:43:47 +0100

    Initial commit. Mother of exceptions

---
+++ AggregateSource.Tests/AggregateSourceExceptionTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using NUnit.Framework;
+
+namespace AggregateSource.Tests {
+  [TestFixture]
+  public class AggregateSourceExceptionTests {
+    [Test]
+    public void IsAnException() {
+      Assert.That(new AggregateSourceException(), Is.InstanceOf<Exception>());
+    }
+
+    [Test]
+    public void UsingTheDefaultConstructorReturnsExceptionWithExpectedProperties() {
+      var sut = new AggregateSourceException();
+
+      //Use of Contains.Substring due to differences in OS.
+      Assert.That(sut.Message, Contains.Substring(typeof(AggregateSourceException).Name));
+      Assert.That(sut.InnerException, Is.Null);
+    }
+
+    [Test]
+    public void UsingTheConstructorWithMessageReturnsExceptionWithExpectedProperties() {
+      var sut = new AggregateSourceException("Message");
+
+      Assert.That(sut.Message, Is.EqualTo("Message"));
+      Assert.That(sut.InnerException, Is.Null);
+    }
+
+    [Test]
+    public void UsingTheConstructorWithMessageAndInnerExceptionReturnsExceptionWithExpectedProperties() {
+      var innerException = new Exception();
+      var sut = new AggregateSourceException("Message", innerException);
+
+      Assert.That(sut.Message, Is.EqualTo("Message"));
+      Assert.That(sut.InnerException, Is.Null);
+    }
+
+    [Test]
+    public void CanBeSerialized() {
+      var innerException = new Exception("InnerMessage");
+      var sut = new AggregateSourceException("Message", innerException);
+      using (var stream = new MemoryStream()) {
+        var formatter = new BinaryFormatter();
+        formatter.Serialize(stream, sut);
+        stream.Position = 0;
+        var result = (AggregateSourceException)formatter.Deserialize(stream);
+        Assert.That(sut.Message, Is.EqualTo(result.Message));
+        Assert.That(sut.InnerException.Message, Is.EqualTo(result.InnerException.Message));
+      }
+    }
+  }
+}
---
+++ AggregateSource/AggregateSourceException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AggregateSource {
+  [Serializable]
+  public class AggregateSourceException : Exception {
+    public AggregateSourceException() {}
+
+    public AggregateSourceException(string message) 
+      : base(message) {}
+
+    public AggregateSourceException(string message, Exception innerException) 
+      : base(message, innerException) {}
+
+    protected  AggregateSourceException(SerializationInfo info, StreamingContext context) : base(info, context) {}
+  }
+}
