6b9778a34d7722d23c355b0f6b6a41c8712ee041
Yves Reynhout <yves.reynhout@gmail.com>
2013-02-13 20:47:16 +0100

    More entity tests

--- AggregateSource.Tests/EntityTests.cs
+++ AggregateSource.Tests/EntityTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace AggregateSource {
@@ -64,9 +65,90 @@ namespace AggregateSource {
     }
 
     [TestFixture]
-    public class WithInstanceWithHandlers {}
+    public class WithInstanceWithHandlers {
+      WithHandlersEntity _sut;
+      Action<object> _applier;
+      List<object> _appliedEvents;
+
+      [SetUp]
+      public void SetUp() {
+        _appliedEvents = new List<object>();
+        _applier = _ => _appliedEvents.Add(_);
+        _sut = new WithHandlersEntity(_applier);
+      }
+
+      [Test]
+      public void PlayCallsHandlerOfEvent() {
+        var expectedEvent = new object();
+
+        _sut.Play(expectedEvent);
+
+        Assert.That(_sut.HandlerCallCount, Is.EqualTo(1));
+        Assert.That(_sut.PlayedEvents, Is.EquivalentTo(new[] {expectedEvent}));
+      }
+
+      [Test]
+      public void ApplyEventCallsApplier() {
+        var @event = new object();
+
+        _sut.DoApply(@event);
+
+        Assert.That(_appliedEvents, Is.EquivalentTo(new[] { @event }));
+      }
+    }
+
+    class WithHandlersEntity : Entity {
+      public WithHandlersEntity(Action<object> applier)
+        : base(applier) {
+        PlayedEvents = new List<object>();
+        Register<object>(@event => {
+          HandlerCallCount++;
+          PlayedEvents.Add(@event);
+        });
+      }
+
+      public void DoApply(object @event) {
+        Apply(@event);
+      }
+
+      public int HandlerCallCount { get; private set; }
+      public List<object> PlayedEvents { get; private set; }
+    }
 
     [TestFixture]
-    public class WithInstanceWithoutHandlers { }
+    public class WithInstanceWithoutHandlers {
+      WithoutHandlersEntity _sut;
+      Action<object> _applier;
+      List<object> _appliedEvents;
+
+      [SetUp]
+      public void SetUp() {
+        _appliedEvents = new List<object>();
+        _applier = _ => _appliedEvents.Add(_);
+        _sut = new WithoutHandlersEntity(_applier);
+      }
+
+      [Test]
+      public void PlayDoesNotThrow() {
+        Assert.DoesNotThrow(() => _sut.Play(new object()));
+      }
+
+      [Test]
+      public void ApplyEventDoesNotThrow() {
+        var @event = new object();
+
+        _sut.DoApply(@event);
+
+        Assert.That(_appliedEvents, Is.EquivalentTo(new[] { @event }));
+      }
+    }
+
+    class WithoutHandlersEntity : Entity {
+      public WithoutHandlersEntity(Action<object> applier) : base(applier) {}
+
+      public void DoApply(object @event) {
+        Apply(@event);
+      }
+    }
   }
 }
---
+++ SampleSource/UsingSelectiveObjectInheritance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleSource {
+  namespace UsingSelectiveObjectInheritance {
+    public class Crate {
+      
+    }
+
+    public class Bottle {
+      
+    }
+  }
+}
