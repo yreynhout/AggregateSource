fe127e030913712d83871b6bb78ea72891cf63c4
Yves Reynhout <yves.reynhout@gmail.com>
2013-04-03 18:34:47 +0200

    Missing files
    - Added missing files (performing a clean checkout revealed this)
    - Fixed the RunMeFirst.bat

--- RunMeFirst.bat
+++ RunMeFirst.bat
@@ -5,6 +5,6 @@ echo Script that downloads and unzips GetEventStore in a specific directory
 echo so that the solution may compile and integration tests may run
 echo ----------------------------------------------------------------------
 
-".nuget/NuGet.exe" install lib/packages.config -o lib
+src\.nuget\NuGet.exe install lib/packages.config -o lib
 "%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /v:n "lib/GEventStoreIntegration.proj" /logger:FileLogger,Microsoft.Build.Engine;LogFile=lib\GEventStoreIntegration.log
 pause
