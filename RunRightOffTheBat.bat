c4bce9021f2b3364b88fbf884080fc0f8b33a819
Yves Reynhout <yves.reynhout@gmail.com>
2013-04-03 21:27:52 +0200

    That message was wrong all along

--- RunRightOffTheBat.bat
+++ RunRightOffTheBat.bat
@@ -2,8 +2,7 @@ cls
 echo off
 echo -------------------------------
 echo Script that builds this project
-echo Please make sure you've run the
-echo RunMeFirst.bat, well, first :-) 
+echo    --- RIGHT OFF THE BAT ---
 echo -------------------------------
 "%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /v:n "build/build.proj" /target:NonInteractiveBuild /logger:FileLogger,Microsoft.Build.Engine;LogFile=build/build.log
 pause
