49eaac894cc34408de9fbbf42cfc1f3a820f4845
Yves Reynhout <yves.reynhout@gmail.com>
2013-04-06 18:42:37 +0200

    Build file tweaking
    - Made downloading GetEventStore integration conditional
    - Moved files around (less in the root is more)

--- RunMeFirst.bat
+++ RunMeFirst.bat
@@ -3,5 +3,5 @@ echo off
 echo ------------------------------------------------
 echo Script that prepares your working copy for usage
 echo ------------------------------------------------
-"%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /v:n "RunMeFirst.proj" /logger:FileLogger,Microsoft.Build.Engine;LogFile=RunMeFirst.log
+"%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /v:n "build\run_me_first.proj" /logger:FileLogger,Microsoft.Build.Engine;LogFile=build\run_me_first.log
 pause
