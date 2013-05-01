b9114c6b23d7c1bcd1c0733e6c102db9f7068c0c
Yves Reynhout <yves.reynhout@gmail.com>
2013-05-01 11:44:07 +0200

    Knock, Knock, Housekeeping
    - StreamSource.FirebirdClient* becoming first class citizen of "THE
    PROCESS"

---
+++ src/StreamSource.FirebirdClient.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StreamSource.FirebirdClient.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StreamSource")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.0.2")]
+[assembly: AssemblyFileVersion("0.0.0.2")]
