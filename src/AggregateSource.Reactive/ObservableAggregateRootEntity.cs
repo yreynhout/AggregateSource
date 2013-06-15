117fbf788bf5769518149abd8a8f88c8fc58fb11
Yves Reynhout <yves.reynhout@gmail.com>
2013-06-15 13:15:58 +0200

    Bye bye Reactive Extensions
    - No longer require the ReplaySubject (which wasn't playing ball anyway
    - needed a resettable ReplaySubject<T>).
    - Still need to make this thread-safe for the async bits and
    multi-threaded subscribers.

--- src/AggregateSource.Reactive/ObservableAggregateRootEntity.cs
+++ src/AggregateSource.Reactive/ObservableAggregateRootEntity.cs
@@ -1,28 +1,50 @@
 ﻿using System;
-using System.Reactive.Subjects;
+using System.Collections.Generic;
 
 namespace AggregateSource.Reactive {
   public class ObservableAggregateRootEntity : AggregateRootEntity, IObservable<object>, IDisposable {
-    readonly ReplaySubject<object> _recorder;
+    readonly List<IObserver<object>> _observers;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="ObservableAggregateRootEntity"/> class.
     /// </summary>
     protected ObservableAggregateRootEntity() {
-      _recorder = new ReplaySubject<object>(Int32.MaxValue, TimeSpan.MaxValue);
-    }
-
-    protected override void Record(object @event) {
-      base.Record(@event);
-      _recorder.OnNext(@event);
+      _observers = new List<IObserver<object>>();
     }
 
     public IDisposable Subscribe(IObserver<object> observer) {
-      return _recorder.Subscribe(observer);
+      if (observer == null) throw new ArgumentNullException("observer");
+      _observers.Add(observer);
+      CatchUp(observer);
+      return new Subscription(() => _observers.Remove(observer));
+    }
+
+    void CatchUp(IObserver<object> observer) {
+      foreach (var change in GetChanges()) {
+        observer.OnNext(change);
+      }
     }
 
     public void Dispose() {
-      _recorder.Dispose();
+      foreach (var observer in _observers) {
+        observer.OnCompleted();
+      }
+    }
+
+    class Subscription : IDisposable {
+      bool _disposed;
+      readonly Action _disposer;
+
+      public Subscription(Action disposer) {
+        _disposer = disposer;
+        _disposed = false;
+      }
+
+      public void Dispose() {
+        if (_disposed) return;
+        _disposed = true;
+        _disposer();
+      }
     }
   }
 }
