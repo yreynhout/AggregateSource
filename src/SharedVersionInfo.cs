edfb0a3759cab85d9ad1e3e183f0b8b6c4008232
Yves Reynhout <yves.reynhout@gmail.com>
2013-07-23 23:55:56 +0200

    Pushed a new package

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("0.0.36.0")]
+[assembly: System.Reflection.AssemblyVersion("0.0.38.0")]
 
 
