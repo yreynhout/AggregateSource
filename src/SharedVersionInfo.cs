fa28212fff57758fc1d7a7c1176388d2044ffceb
Yves Reynhout <yreynhout@csc.com>
2013-07-24 19:12:33 +0200

    NuGet - Depend on ES 2.0 client

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("0.0.47.0")]
+[assembly: System.Reflection.AssemblyVersion("0.0.48.0")]
 
 
