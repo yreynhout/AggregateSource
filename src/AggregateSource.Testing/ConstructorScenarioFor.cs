17f76c40cd68e736acb8b17fc8fd3230f9ffb40c
Yves Reynhout <yves.reynhout@gmail.com>
2013-07-28 00:24:37 +0200

    Minor tweaking
    and build number bumping

--- src/AggregateSource.Testing/ConstructorScenarioFor.cs
+++ src/AggregateSource.Testing/ConstructorScenarioFor.cs
@@ -6,21 +6,22 @@ namespace AggregateSource.Testing
 	/// A when-then test specification bootstrapper for testing an aggregate constructor,
 	/// i.e. a method on the aggregate that returns an instance of the aggregate.
 	/// </summary>
-	public class ConstructorScenarioFor : IAggregateConstructorWhenStateBuilder
+    public class ConstructorScenarioFor<TAggregateRoot> : IAggregateConstructorWhenStateBuilder
+        where TAggregateRoot : IAggregateRootEntity
 	{
-		private readonly Func<IAggregateRootEntity> _ctor;
+		private readonly Func<IAggregateRootEntity> _constructor;
 
 		/// <summary>
 		/// Creates a new constructor scenario testing the constructor passed as a delegate.
 		/// </summary>
-		/// <param name="ctor">The constructor delegate.</param>
+		/// <param name="constructor">The constructor invocation.</param>
 		/// <exception cref="ArgumentNullException">If the constructor delegate is null.</exception>
-		public ConstructorScenarioFor(Func<IAggregateRootEntity> ctor)
+        public ConstructorScenarioFor(Func<TAggregateRoot> constructor)
 		{
-			if (ctor == null)
-				throw new ArgumentNullException("ctor");
+			if (constructor == null)
+				throw new ArgumentNullException("constructor");
 
-			_ctor = ctor;
+			_constructor = () => constructor();
 		}
 
 		/// <summary>
@@ -31,7 +32,7 @@ namespace AggregateSource.Testing
 		public IAggregateConstructorThenStateBuilder Then(params object[] events)
 		{
 			if (events == null) throw new ArgumentNullException("events");
-			return new AggregateConstructorThenStateBuilder(_ctor, events);
+			return new AggregateConstructorThenStateBuilder(_constructor, events);
 		}
 
 		/// <summary>
@@ -42,7 +43,7 @@ namespace AggregateSource.Testing
 		public IAggregateConstructorThrowStateBuilder Throws(Exception exception)
 		{
 			if (exception == null) throw new ArgumentNullException("exception");
-			return new AggregateConstructorThrowStateBuilder(_ctor, exception);
+			return new AggregateConstructorThrowStateBuilder(_constructor, exception);
 		}
 	}
 }
