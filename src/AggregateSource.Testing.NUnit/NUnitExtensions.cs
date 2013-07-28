5ea79d4e83100320671132fda97955fa0265fc09
James Nugent <james.nugent@bathasu.com>
2013-07-28 18:41:32 +0100

    Added XML documentation to AS.Testing.NUnit
    Added XML comments to all public members
    Changed project file to output XML docs on build in Debug and Release
    configurations

--- src/AggregateSource.Testing.NUnit/NUnitExtensions.cs
+++ src/AggregateSource.Testing.NUnit/NUnitExtensions.cs
@@ -3,8 +3,15 @@ using NUnit.Framework;
 
 namespace AggregateSource.Testing
 {
+	/// <summary>
+	/// Extension methods for running AggregateSource.Testing specifications using NUnit.
+	/// </summary>
 	public static class NUnitExtensions
 	{
+		/// <summary>
+		/// Runs an event-centric aggregate factory specification using NUnit.
+		/// </summary>
+		/// <param name="builder">The specification builder.</param>
 		public static void Assert(this IAggregateFactoryThenStateBuilder builder)
 		{
 			var specification = builder.Build();
@@ -16,6 +23,12 @@ namespace AggregateSource.Testing
 				Is.EquivalentTo(specification.Thens));
 		}
 
+		/// <summary>
+		/// Runs an exception-centric aggregate factory specification using NUnit.
+		/// </summary>
+		/// <typeparam name="TException">Type of the expected exception.</typeparam>
+		/// <param name="builder">The specification builder.</param>
+		/// <param name="exception">An instance equivalent to the expected exception.</param>
 		public static void AssertThrows<TException>(this IAggregateFactoryWhenStateBuilder builder,
 													TException exception) where TException : Exception
 		{
@@ -27,6 +40,10 @@ namespace AggregateSource.Testing
 				Is.EqualTo(exception.Message));
 		}
 
+		/// <summary>
+		/// Runs an event-centric aggregate constructor specification using NUnit.
+		/// </summary>
+		/// <param name="builder">The specification builder.</param>
 		public static void Assert(this IAggregateConstructorThenStateBuilder builder)
 		{
 			var specification = builder.Build();
@@ -36,6 +53,12 @@ namespace AggregateSource.Testing
 				Is.EquivalentTo(specification.Thens));
 		}
 
+		/// <summary>
+		/// Runs an exception-centric aggregate constructor specification using NUnit.
+		/// </summary>
+		/// <typeparam name="TException">Type of the expected exception.</typeparam>
+		/// <param name="builder">The specification builder.</param>
+		/// <param name="exception">An instance equivalent to the expected exception.</param>
 		public static void AssertThrows<TException>(this IAggregateConstructorWhenStateBuilder builder,
 													TException exception) where TException : Exception
 		{
@@ -45,6 +68,10 @@ namespace AggregateSource.Testing
 				Is.EqualTo(exception.Message));
 		}
 
+		/// <summary>
+		/// Runs an event-centric aggregate command specification using NUnit.
+		/// </summary>
+		/// <param name="builder">The specification builder.</param>
 		public static void Assert(this IAggregateCommandThenStateBuilder builder)
 		{
 			var specification = builder.Build();
@@ -56,6 +83,12 @@ namespace AggregateSource.Testing
 				Is.EquivalentTo(specification.Thens));
 		}
 
+		/// <summary>
+		/// Runs an exception-centric aggregate command specification using NUnit.
+		/// </summary>
+		/// <typeparam name="TException">Type of the expected exception.</typeparam>
+		/// <param name="builder">The specification builder.</param>
+		/// <param name="exception">An instance equivalent to the expected exception.</param>
 		public static void AssertThrows<TException>(this IAggregateCommandWhenStateBuilder builder,
 													TException exception) where TException : Exception
 		{
@@ -67,6 +100,10 @@ namespace AggregateSource.Testing
 				Is.EqualTo(exception.Message));
 		}
 
+		/// <summary>
+		/// Runs an event-centric aggregate query specification using NUnit.
+		/// </summary>
+		/// <param name="builder"></param>
 		public static void Assert(this IAggregateQueryThenStateBuilder builder)
 		{
 			var specification = builder.Build();
@@ -78,6 +115,13 @@ namespace AggregateSource.Testing
 				Is.EqualTo(specification.Then));
 		}
 
+		/// <summary>
+		/// Runs an exception-centric aggregate query specification using NUnit.
+		/// </summary>
+		/// <typeparam name="TException">Type of the expected exception.</typeparam>
+		/// <typeparam name="TResult">Type of the query result.</typeparam>
+		/// <param name="builder">The specification builder.</param>
+		/// <param name="exception">An instance equivalent to the expected exception.</param>
 		public static void AssertThrows<TResult, TException>(this IAggregateQueryWhenStateBuilder<TResult> builder,
 															 TException exception) where TException : Exception
 		{
