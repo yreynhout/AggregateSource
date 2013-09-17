86c6c159212a5842d390c612d38f9f407585b2c1
Yves Reynhout <yves.reynhout@gmail.com>
2013-09-17 23:20:41 +0200

    See what typos make me do

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("0.0.115.0")]
+[assembly: System.Reflection.AssemblyVersion("0.0.116.0")]
 
 
