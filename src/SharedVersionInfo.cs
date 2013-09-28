68f982b5c9725ea17e9a21ae6599b3a74b1acdd5
Yves Reynhout <yves.reynhout@gmail.com>
2013-09-28 19:15:20 +0200

    Published
    but that wasn't the smartest of ideas (package pulled)

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("0.0.132.0")]
+[assembly: System.Reflection.AssemblyVersion("0.0.133.0")]
 
 
