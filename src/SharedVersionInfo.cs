0fa3ff6cf2854758ec365b528dc1db011ab22b08
Yves Reynhout <yves.reynhout@gmail.com>
2013-10-03 22:10:35 +0200

    Added missing nuget feed
    for NEventStore (ci). Will ditch it once it's on the NuGet feed.

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("0.0.156.0")]
+[assembly: System.Reflection.AssemblyVersion("0.0.157.0")]
 
 
