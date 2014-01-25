7b950c1679db4f13e49f21d7ee18123a10252452
Yves Reynhout <yves.reynhout@gmail.com>
2014-01-25 23:25:43 +0100

    Added proposed multi-tenant repository signature

---
+++ src/Core/AggregateSource.Core/ITenantAsyncRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+
+namespace AggregateSource
+{
+    /// <summary>
+    /// Represents an asynchronous, virtual collection of <typeparamref name="TAggregateRoot"/> for a particular tenant.
+    /// </summary>
+    /// <typeparam name="TAggregateRoot">The type of the aggregate root in this collection.</typeparam>
+    public interface ITenantAsyncRepository<TAggregateRoot>
+    {
+        /// <summary>
+        /// Gets the aggregate root entity associated with the specified aggregate identifier.
+        /// </summary>
+        /// <param name="tenant">The tenant.</param>
+        /// <param name="identifier">The aggregate identifier.</param>
+        /// <returns>An instance of <typeparamref name="TAggregateRoot"/>.</returns>
+        /// <exception cref="AggregateNotFoundException">Thrown when an aggregate is not found.</exception>
+        Task<TAggregateRoot> GetAsync(string tenant, string identifier);
+
+        /// <summary>
+        /// Attempts to get the aggregate root entity associated with the aggregate identifier.
+        /// </summary>
+        /// <param name="tenant">The tenant.</param>
+        /// <param name="identifier">The aggregate identifier.</param>
+        /// <returns>The found <typeparamref name="TAggregateRoot"/>, or empty if not found.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures")]
+        Task<Optional<TAggregateRoot>> GetOptionalAsync(string tenant, string identifier);
+
+        /// <summary>
+        /// Adds the aggregate root entity to this collection using the specified aggregate identifier.
+        /// </summary>
+        /// <param name="tenant">The tenant.</param>
+        /// <param name="identifier">The aggregate identifier.</param>
+        /// <param name="root">The aggregate root entity.</param>
+        void Add(string tenant, string identifier, TAggregateRoot root);
+    }
+}
