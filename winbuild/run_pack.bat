6a338ef59e36b9a999284c1785886450982d8bdb
Yves Reynhout <yves.reynhout@gmail.com>
2014-01-26 16:18:03 +0100

    CI nuget feed integration
    - Integrated the myget feed into the CI build process

--- winbuild/run_pack.bat
+++ winbuild/run_pack.bat
@@ -5,5 +5,5 @@ echo Script that builds this project
 echo Please make sure you've run the
 echo RunMeFirst.bat, well, first :-) 
 echo -------------------------------
-"%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /v:n "nuget.proj" /target:Pack /logger:FileLogger,Microsoft.Build.Engine;LogFile=nuget.log
+"%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /v:n "nuget.proj" /target:Pack /logger:FileLogger,Microsoft.Build.Engine;LogFile=pack.log
 pause
--- winbuild/run_push_ci.bat
+++ winbuild/run_push_ci.bat
@@ -6,5 +6,5 @@ echo Please make sure you've run the
 echo RunMeFirst.bat, well, first :-) 
 echo -------------------------------
 set /p apikey=Enter NuGet ApiKey:
-"%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /v:n "nuget.proj" /target:Push /p:NuGetApiKey=%apikey% /logger:FileLogger,Microsoft.Build.Engine;LogFile=psuh.log
+"%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /v:n "nuget.proj" /target:PushCI /p:NuGetApiKey=%apikey% /logger:FileLogger,Microsoft.Build.Engine;LogFile=pushci.log
 pause
