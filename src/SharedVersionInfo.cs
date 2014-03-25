9a5ee72d7335df5062ce4581dd09212cdeff2021
Yves Reynhout <yves.reynhout@gmail.com>
2014-03-25 21:21:15 +0100

    Fixed broken project

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -8,5 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("0.0.294.0")]
+[assembly: System.Reflection.AssemblyVersion("0.0.297.0")]
+
 
